85b05f88303af1336fc27c93c092d518f297df31
saavaj <mishaludeshi96@gmail.com>
2021-06-19 11:14:20 +0530

    Initail Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Saavaj on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
