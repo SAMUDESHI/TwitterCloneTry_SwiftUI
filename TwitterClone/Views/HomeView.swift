5dacc4eb252a73a0561c85d77fef6d89fa63d9d4
Saavaj Studios <saavajgroup@192.168.1.3>
2023-06-30 17:41:47 +0530

    Change TO MVVM & Initla View Setup

---
+++ TwitterClone/Views/HomeView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Saavaj on 19/06/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+
+        if #available(iOS 16.0, *) {
+            NavigationStack{
+        
+                TabView{
+                    Text("Feed")
+                        .tabItem{
+                            Image(systemName: "house")
+                            Text("Home")
+                        }
+                    Text("Search")
+                        .tabItem{
+                            Image(systemName: "magnifyingglass")
+                            Text("Search")
+                        }
+                    Text("Message")
+                        .tabItem{
+                            Image(systemName: "envelope")
+                            Text("Message")
+                        }
+                }
+            }
+        } else {
+            NavigationView{
+                TabView{
+                    Text("Feed")
+                        .tabItem{
+                            Image(systemName: "house")
+                            Text("Home")
+                        }
+                    Text("Search")
+                        .tabItem{
+                            Image(systemName: "magnifyingglass")
+                            Text("Search")
+                        }
+                    Text("Message")
+                        .tabItem{
+                            Image(systemName: "envelope")
+                            Text("Message")
+                        }
+                }
+            }
+            // Fallback on earlier versions
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
